858f1c8ed391aec4244c53cc4696a94e07d85b63
GameBuilder101 <gamebuilder101@gmail.com>
2024-10-26 18:15:42 -0400

    Added GameOver stats

--- game-over/GameOver.cs
+++ game-over/GameOver.cs
@@ -8,6 +8,20 @@ public partial class GameOver : Control
     [Export]
     private string _titleScreen;
 
+    [Export]
+    private RichTextLabel _statsLabel;
+
+    public static int Score { get; set; }
+    public static double TimeSurvived { get; set; }
+    public static int AsteroidsCollected { get; set; }
+    public static int AsteroidsShot { get; set; }
+
+    public override void _Ready()
+    {
+        base._Ready();
+        _statsLabel.Text = $"Score: {Score}\nTime Survived: {TimeSurvived}\nAsteroids Collected: {AsteroidsCollected}\nAsteroids Shot: {AsteroidsShot}";
+    }
+
     public void Restart()
     {
         GetTree().CallDeferred("change_scene_to_file", _gameplay);
