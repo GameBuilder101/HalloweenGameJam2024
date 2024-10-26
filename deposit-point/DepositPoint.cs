46262d6159874681929ee59cf10f83eb81ccd2e6
Auden Clifford <awpclifford@gmail.com>
2024-10-26 14:50:10 -0400

    merge commit

---
+++ deposit-point/DepositPoint.cs
@@ -0,0 +1,32 @@
+using Godot;
+using System;
+
+[GlobalClass]
+public partial class DepositPoint : RigidBody2D
+{
+	[Export]
+	private float _speed;
+	[Export]
+	public float OrbitRadius { get; set; }
+	[Export]
+	public float CurrentObritAngle { get; private set; }
+
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		base._Ready();
+		CurrentObritAngle *= Mathf.Pi;
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+		base._Process(delta);
+		CurrentObritAngle -= _speed * (float)delta;
+
+        Vector2 newPos = new Vector2(Mathf.Cos(CurrentObritAngle), Mathf.Sin(CurrentObritAngle)) * OrbitRadius;
+        MoveAndCollide(newPos - Position);
+		Position = newPos;
+		Rotation = CurrentObritAngle - Mathf.Pi * 0.5f;
+	}
+}
