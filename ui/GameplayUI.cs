6b3792640a5829553437e84992d06e6182ecb1a2
Lyx Huston <lyx@hustonfam.net>
2024-10-26 21:55:50 -0400

    fuel bar yellow

--- ui/GameplayUI.cs
+++ ui/GameplayUI.cs
@@ -9,7 +9,12 @@ public partial class GameplayUI : Control
 	private RichTextLabel _pickUpLabel;
     [Export]
     private RichTextLabel _dropOffLabel;
-
+	[Export]
+	private Color[] Colors;
+	[Export]
+	private StyleBoxFlat background;
+	[Export]
+	private StyleBoxFlat bar;
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
 	{
@@ -22,6 +27,10 @@ public partial class GameplayUI : Control
 
         _fuelBar.MaxValue = player.MaxFuel;
 		_fuelBar.Value = player.Fuel;
+		_fuelBar.Indeterminate = player.IsSpinningOut;
+		Color color = Colors[player.IsSpinningOut ? 1 : 0];
+		background.BorderColor = color;
+		bar.BgColor = color;
 		_pickUpLabel.Visible = player.InRadiusOfAsteroid && player.PickedUpAsteroid == null && !player.IsSpinningOut;
 		_dropOffLabel.Visible = player.IsInDropOffRadius && player.PickedUpAsteroid != null && !player.IsSpinningOut;
 	}
