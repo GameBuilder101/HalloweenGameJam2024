1949e236b93e6effc9a2a9410d13dfe2d5f28d0e
Auden-Clifford <awpclifford@gmail.com>
2024-10-27 14:41:51 -0400

    fixed debris particles

--- asteroids/Asteroid.cs
+++ asteroids/Asteroid.cs
@@ -4,56 +4,60 @@ using System;
 [GlobalClass]
 public partial class Asteroid : RigidBody2D, IDamageable
 {
-    public float Health { get; private set; } = 1.0f;
-    [Export]
-    public double SpinOutDuration { get; set; } = 4.0;
+	public float Health { get; private set; } = 1.0f;
+	[Export]
+	public double SpinOutDuration { get; set; } = 4.0;
 	[Export] private PackedScene FuelScene;
 
-    [Export]
-    public int Score { get; set; } = 100;
-    [Export]
-    public int DepositBonusFuel { get; set; } = 0;
+	[Export]
+	public int Score { get; set; } = 100;
+	[Export]
+	public int DepositBonusFuel { get; set; } = 0;
 
-    [Export]
-    private GpuParticles2D _killParticles;
+	[Export]
+	public int FuelMultiplier { get; set; } = 1;
 
-    // Called when the node enters the scene tree for the first time.
-    public override void _Ready()
+	[Export]
+	private GpuParticles2D _killParticles;
+
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
 	{
-        base._Ready();
-        GameManager.Instance.Asteroids.Add(this);
-    }
-
-    public override void _ExitTree()
-    {
-        base._ExitTree();
-        GameManager.Instance.Asteroids.Remove(this);
-    }
-
-    // Called every frame. 'delta' is the elapsed time since the previous frame.
-    public override void _Process(double delta)
-	{
-        base._Process(delta);
-
-        // if the asteroid goes outside the game radius, destroy it
-        if (Position.Length() > GameManager.Instance.GameBoundsRadius + 800.0f) 
-        {
-            QueueFree();
-        }
+		base._Ready();
+		GameManager.Instance.Asteroids.Add(this);
 	}
 
-    public void ChangeHealth(float value)
-    {
-        Health += value;
-        if (Health <= 0.0f)
-            Kill();
-    }
+	public override void _ExitTree()
+	{
+		base._ExitTree();
+		GameManager.Instance.Asteroids.Remove(this);
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+		base._Process(delta);
+
+		// if the asteroid goes outside the game radius, destroy it
+		if (Position.Length() > GameManager.Instance.GameBoundsRadius + 800.0f) 
+		{
+			GameManager.Instance.Asteroids.Remove(this);
+			QueueFree();
+		}
+	}
+
+	public void ChangeHealth(float value)
+	{
+		Health += value;
+		if (Health <= 0.0f)
+			Kill();
+	}
 
 	private static RandomNumberGenerator rng = new RandomNumberGenerator();
 
-    public void Kill()
-    {
-		int numFuel = (int) (_Scale * 5);
+	public void Kill()
+	{
+		int numFuel = (int) (_Scale * 5 * FuelMultiplier);
 		float maxVel = (_Scale * 100);
 		for (int i = 0; i < numFuel; i++) {
 			Fuel fuel = (Fuel) FuelScene.Instantiate();
@@ -65,17 +69,17 @@ public partial class Asteroid : RigidBody2D, IDamageable
 			GetParent().CallDeferred("add_child", fuel);
 		}
 
-        _killParticles.Reparent(GetTree().Root);
-        _killParticles.Emitting = true;
-        QueueFree();
-    }
+		_killParticles.Reparent(GetTree().Root);
+		_killParticles.Emitting = true;
+		QueueFree();
+	}
 
-    private void PickUpAreaBodyEntered(Node node)
-    {
-        if (Freeze || !(node is Player))
-            return;
-        ((Player)node).SpinOut(SpinOutDuration);
-    }
+	private void PickUpAreaBodyEntered(Node node)
+	{
+		if (Freeze || !(node is Player))
+			return;
+		((Player)node).SpinOut(SpinOutDuration);
+	}
 	
 	private float _Scale = 0.0f;
 	
