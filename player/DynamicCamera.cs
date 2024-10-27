d7e9b45f0b348326bc929940325d2b37aaa414bd
GameBuilder101 <gamebuilder101@gmail.com>
2024-10-27 11:15:22 -0400

    Fix dynamic shake error

--- player/DynamicCamera.cs
+++ player/DynamicCamera.cs
@@ -7,8 +7,13 @@ public partial class DynamicCamera : Camera2D
 	[Export]
 	private Node2D TrackTarget { get; set; }
 
-	public float CurrentShakeDuration;
-	public float CurrentShakeDuration;
+	[Export]
+	private float _ambientShakeIntensity;
+	public float AmbientShakeScale { get; private set; }
+
+	private double _shakeTime;
+	private double _shakeDuration = 1.0;
+	private float _shakeIntensity;
 
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
@@ -18,9 +23,21 @@ public partial class DynamicCamera : Camera2D
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
-		Vector2 pos = Position;
+        Vector2 pos = Position;
         pos.X = TrackTarget.Position.X;
         pos.Y = TrackTarget.Position.Y;
-		Position = pos;
+        Position = pos;
+
+        AmbientShakeScale = Mathf.Clamp(1.0f - TrackTarget.Position.Length() / GameManager.Instance.BlackHole.BlackHoleRadius * 4.0f, 0.0f, 1.0f);
+		foreach (Enemy enemy in EnemyManager.Instance.Enemies)
+            AmbientShakeScale += Mathf.Clamp(1.0f - TrackTarget.Position.DistanceTo(enemy.Position) / 800.0f, 0.0f, 1.0f) * 0.5f;
+        AmbientShakeScale = Mathf.Clamp(AmbientShakeScale, 0.0f, 1.0f);
+
+		_shakeTime -= delta;
+		if (_shakeTime < 0.0)
+			_shakeTime = 0.0;
+
+		float combinedIntensity = AmbientShakeScale * _ambientShakeIntensity + (float)(_shakeTime / _shakeDuration) * _shakeIntensity;
+
     }
 }
